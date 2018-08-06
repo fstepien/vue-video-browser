b2c4dff30902d319c2ad6eb1bf94e030253d0b81
Filip Stepien <contact@filipstepien.com>
2018-08-06 19:52:24 -0400

    add video list and video list items

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <template>
-    <div>Hi there!</div>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList :videos="videos"></VideoList>
+    </div>
 </template>
 
 <script>
+import SearchBar from "./components/SearchBar"
+import VideoList from "./components/VideoList"
+import axios from 'axios'
+const API_KEY = 'AIzaSyC1UfAuWRUzj1ryFCXIc6XBERsdg6knNnE'
+
 export default {
-name: 'App'
+name: 'App',
+components: {
+    SearchBar,
+    VideoList
+}, 
+data() {
+return {
+    videos: []
+}
+},
+methods: {
+    onTermChange(searchTerm) {
+        axios.get('https://www.googleapis.com/youtube/v3/search', {
+            headers: {'Access-Control-Allow-Origin': '*'},
+            dataType: 'jsonp',
+            params: {
+                key: API_KEY, 
+                type: 'video', 
+                part: 'snippet',
+                q: searchTerm
+            }
+        }).then(response => this.videos = response.data.items)
+    }
+}
+
 }
 </script>
 
---
+++ src/components/VideoList.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="list-group">
+        <VideoListItem v-for="video in videos" :video="video" :key="video.etag"></VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from "./VideoListItem"
+export default {
+    name: 'VideoList',
+    components: {
+        VideoListItem
+    },
+    props: {
+        videos: {
+            type: Array, 
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+</style>
